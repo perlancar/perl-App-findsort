0f3cdb97d9400ca530961d672c738c23fe831424
perlancar (on notebook-dell-xps13-9360-dec2017) <perlancar@gmail.com>
2024-09-19 14:21:11 +0700

    Initial commit

---
+++ lib/App/findsort.pm
@@ -0,0 +1,15 @@
+package App::findsort;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Unix find wrapper to add sorting
+
+=head1 SYNOPSIS
+
+See included script L<findsort>.
